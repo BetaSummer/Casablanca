988c27e7573e8578a0a736882d2fa1b54f663c83
clarkzsd <clarkzsd@gmail.com>
2017-07-17 13:16:36 +0800

    member list UI improvements

--- src/components/MemberCard.vue
+++ src/components/MemberCard.vue
@@ -12,8 +12,12 @@
       </span>
     </div>
     <div class="social-media">
-      <i class="iconfont icon-pinnedoctocat"></i>
-      <i class="iconfont icon-blog"></i>
+      <a href="">
+        <i class="iconfont icon-pinnedoctocat"></i>
+      </a>
+      <a href="">
+        <i class="iconfont icon-blog"></i>
+      </a>
     </div>
   </div>
 </template>
@@ -22,16 +26,41 @@
 
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
 .member-card
   display flex
   flex-direction column
   align-items center
-  min-width 300px
-  min-height 350px
-  border 1px solid black
+  justify-content space-around
+  min-width 280px
+  min-height 370px
+  margin 15px
+  border-radius 4px
+  background-color white
+  .member-info
+    display flex
+    flex-direction column
+    align-items center
+
+.avatar
   img
-    width 50px
-    height 50px
+    width 100px
+    height 100px
     border-radius 50%
+
+span.name
+  font-weight bold
+  font-size 1.3rem
+
+span.identity
+  color #828282
+  font-size 0.9rem
+
+.social-media
+  a
+    color #fff
+    text-decoration none
+    outline none
+  .iconfont
+    color black
 </style>
--- src/components/MemberList.vue
+++ src/components/MemberList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="member-list">
-    <member-card v-for="item in [1,2,3,4,5]" :key="item"></member-card>
+    <member-card v-for="item in [1,2,3,4,5,6,7,8,9,10]" :key="item"></member-card>
   </div>
 </template>
 
@@ -18,7 +18,9 @@ export default {
 .member-list
   flex 1
   display flex
+  align-items center
   flex-wrap wrap
+  justify-content center
   height calc(100vh - 100px - 2rem)
   overflow-y scroll
 </style>
