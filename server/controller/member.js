259a108c6b994a3156eda28f05d1ae7979df2645
clarkzsd <clarkzsd@gmail.com>
2017-08-01 16:02:28 +0800

    refactor[service]: remove useless code

--- server/controller/member.js
+++ server/controller/member.js
@@ -2,8 +2,9 @@ const memberService = require('../service/member');
 
 class MemberController {
   async index(ctx) {
-    const generation = ctx.query.generation || '';
-    const members = generation === '' ? await memberService.findAll(generation) : await memberService.findByGeneration(generation);
+    const generation = ctx.query.generation;
+    const members = ctx.query.generation ? await memberService.findByGeneration(generation) :
+    await memberService.findAll();
     ctx.body = members;
   }
 }
