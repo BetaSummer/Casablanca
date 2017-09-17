f464d30f00d70bef7ecd281221d6a72d40bb8ce6
clarkzsd <zsd1996@hotmail.com>
2017-09-17 23:17:21 +0800

    feat[auth]: add jwt auth middleware to protect apis

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
 const cors = require('koa-cors');
+const jwt = require('jsonwebtoken');
+const secret = require('./config/config.dev.js').secret;
 
 const router = require('./router');
 
 const app = new Koa();
 app.use(cors());
 app.use(bodyparser());
+
+app.use((ctx, next) => {
+  if (ctx.header && ctx.header.authorization) {
+    try {
+      jwt.verify(ctx.header.authorization, secret);
+      return next();
+    } catch (e) {
+      ctx.status = 401;
+      ctx.body = 'Protected resource, use correct Authorization header to get access';
+    }
+  } else {
+    ctx.status = 401;
+    ctx.body = 'Protected resource, use Authorization header to get access';
+  }
+});
+
 app.use(router.routes());
 
 app.listen('3001');
