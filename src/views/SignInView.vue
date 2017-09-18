dc3d07aea345581630d9ba5c00f443edcfb17ff3
clarkzsd <zsd1996@hotmail.com>
2017-09-18 18:56:34 +0800

    refactor[view]: refactor components displaying structure

---
+++ src/views/SignInView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignInView',
+};
+</script>
+
+<style lang="stylus" scoped>
+.container
+  background url('../assets/background.jpg')
+</style>
